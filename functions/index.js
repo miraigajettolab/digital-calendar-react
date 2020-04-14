bb8acde8923eb118dc2b08a51000e3c59397b9a1
Sergey Grigorenko <miraigajettolab@gmail.com>
2020-04-14 05:44:19 +0300

    add cloud functions to handle indices for fulltext firestore search with algolia

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,27 @@
 const functions = require('firebase-functions');
+const algoliasearch = require('algoliasearch');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const APP_ID = functions.config().algolia.app;
+const ADMIN_KEY = functions.config().algolia.key;
+
+const client = algoliasearch(APP_ID, ADMIN_KEY);
+const index = client.initIndex('users');
+
+exports.addToUsersIndex = functions.firestore.document('users/{userId}')
+    .onCreate(snapshot => {
+        const data = snapshot.data();
+        const objectID = snapshot.id;
+        return index.addObject({...data, objectID});
+    });
+
+exports.updateUsersIndex = functions.firestore.document('users/{userId}')
+    .onUpdate(change => {
+        const newData = change.after.data();
+        const objectID = change.after.id;
+        return index.saveObject({...newData, objectID});
+    });
+
+exports.deleteFromUsersIndex = functions.firestore.document('users/{userId}')
+    .onDelete(snapshot => {
+        index.deleteObject(snapshot.id)
+    });
