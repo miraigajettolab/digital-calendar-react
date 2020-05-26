8de1eac82258cd193bcbc4a1f6a93fae84da6c77
Sergey Grigorenko <miraigajettolab@gmail.com>
2020-05-26 03:42:51 +0300

    Implement conditional rendering in Default panel

--- src/panels/default/Default.js
+++ src/panels/default/Default.js
@@ -2,24 +2,41 @@ import React from "react"
 import "./default.css"
 import TopNavBar from './navbar.js';
 import AddEvent from "../addEvent/addEvent"
+import MonthViewer from "../monthViewer/MonthViewer"
+import * as firebase from "firebase"
 
 
 class Default extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
+          user: ""
           };
     }
+
     render() {
+      const firestore = firebase.firestore();
+      const authUser = firebase.auth().currentUser;
+      if(authUser){
+          const userRef = firestore.doc("users/"+ authUser.email)
+          userRef.get().then((doc) => {
+              if(doc && doc.exists){
+                  this.setState({user:doc.data()})
+              }
+          }).catch(e => console.log(e.message))
+      }
+      let addEventVar = <AddEvent/>;
+      let monthViewerVar = <MonthViewer/>
+    
     return (
       <nav>
           <TopNavBar activePanelHandler = {this.props.activePanelHandler}/>
             <div className="contentColumn">
               <div className="stubRow">
-                    <AddEvent/>
+                {this.state.user.isEventManager?addEventVar:monthViewerVar}
               </div>
             </div>
-          </nav>
+      </nav>
         )
     }
 }
